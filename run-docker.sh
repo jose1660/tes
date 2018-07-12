4bdc4293498857077e53c470761be53e229084be
Antonio <jose1660@gmail.com>
2018-07-12 14:42:42 -0500

    cambios

--- run-docker.sh
+++ run-docker.sh
@@ -6,8 +6,7 @@ export EXPRESS_PORT=9999
 export HUBOT_SLACK_TOKEN=xoxb-220558913360-384990725124-AXHVoSwavi6nQjVS2701Gbt0
 export HUBOT_ADAPTER=slack
 export HUBOT_JENKINS_URL=http://ip172-18-0-23-bd3qqsts2ti0008pm780-8080.direct.labs.play-with-docker.com
-export HUBOT_JENKINS_AUTH=admin:admin
-
+export HUBOT_JENKINS_AUTH=chatops:09557eb3f90f502fbc305498ce350ff1
 
 docker run \
   -e "HUBOT_SLACK_TOKEN=$HUBOT_SLACK_TOKEN" \
