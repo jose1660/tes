d780dd4a4706c38530a972ddb55a8589378f76a4
jose1660 <jose1660@gmail.com>
2019-10-02 14:16:12 -0500

    Update run-docker.sh

--- run-docker.sh
+++ run-docker.sh
@@ -3,7 +3,7 @@
 # This is just an example on how we can run hubot with docker.
 export PORT=9999
 export EXPRESS_PORT=9999
-export HUBOT_SLACK_TOKEN=xoxb-220558913360-384990725124-AXHVoSwavi6nQjVS2701Gbt0
+export HUBOT_SLACK_TOKEN=xoxb-435859165350-781344934096-ftR8QdUybPw2FQK4khIcRUS6
 export HUBOT_ADAPTER=slack
 export HUBOT_JENKINS_URL=http://52.2.27.172:9091
 export HUBOT_JENKINS_AUTH=chatops:a79d9b8eaa4f8a1731ebf9e59ca19a19
